1f1341a6733e3ee700c4b05f30de8b8b332354b0
dhanu6799 <neelapudhanu@gmail.com>
2020-07-12 17:08:10 +0530

    updated functionalities

--- RDIP/Codes/jquery.js
+++ RDIP/Codes/jquery.js
@@ -1,17 +1,16 @@
-var english = {
-    "line_1" : ["John ate an apple before afternoon","before afternoon John ate an apple","John before afternoon ate an apple"],
-    "line_2"  : ["some students like to study in the night","at night some students like to study"],
-    "line_3" : ["John and Mary went to church","Mary and John went to church"],
-    "line_4" : ["John went to church after eating","after eating John went to church","John after eating went to church"],
-    "line_5" : ["did he go to market","he did go to market"],
-    "line_6" : ["the woman who called my sister sells cosmetics","the woman who sells cosmetics called my sister","my sister who sells cosmetics called the woman","my sister who called the woman sells cosmetics"],
-    "line_7" : ["John goes to the library and studies","John studies and goes to the library"],
-    "line_8" : ["John ate an apple so did she","she ate an apple so did John"],
-    "line_9" : ["the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book","after the teacher returned the book she noticed the error","after the teacher noticed the error she returned the book","she returned the book after the teacher noticed the error","she noticed the error after the teacher returned the book","after she returned the book the teacher noticed the error","after she noticed the error the teacher returned the book"],
- "line_10" : ["I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her","yesterday I bought a book that I told her"],
-};
+var english = '{ "con1":[' +'{"a":"John ate an apple before afternoon", "b":"before afternoon John ate an apple", "c":"John before afternoon ate an apple"},' +
+       '{"a":"some students like to study in the night", "b":"at night some students like to study" },' +
+        '{"a":"John and Mary went to church", "b":"Mary and John went to church" },' +
+         '{"a":"John went to church after eating", "b":"after eating John went to church", "c":"John after eating went to church" },' +
+        '{"a":"did he go to market", "b":"he did go to market" },' +
+      '{"a":"the woman who called my sister sells cosmetics", "b":"the woman who sells cosmetics called my sister", "c":"my sister who sells cosmetics called the woman", "d":"my sister who called the woman sells cosmetics" },' +
+      '{"a":"John goes to the library and studies", "b":"John studies and goes to the library" },' +
+    '{"a":"John ate an apple so did she", "b":"she ate an apple so did John" },' +
+   '{"a":"the teacher returned the book after she noticed the error", "b":"the teacher noticed the error after she returned the book<br>", "c":"after the teacher returned the book she noticed the error", "d":"after the teacher noticed the error she returned the book", "e":"she returned the book after the teacher noticed the error", "f":"she noticed the error after the teacher returned the book", "g":"after she returned the book the teacher noticed the error", "h":"after she noticed the error the teacher returned the book" },' +
+     '{"a":"I told her that I bought a book yesterday", "b":"I told her yesterday that I bought a book", "c":"yesterday I told her that I bought a book", "d":"I bought a book that I told her yesterday", "e" :"I bought a book yesterday that I told her", "f" :"yesterday I bought a book that I told her" }]}';
+   
       var a=Math.floor(Math.random() * 9);
-obj=JSON.parse(text);
+obj=JSON.parse(english);
 var b=obj.con1[a].a;
 var c=b.split(" ");
 var d=c.length
@@ -26,6 +25,7 @@ m++;
 }
 while (d!=g.length);
 for(var i=0;i<d;i++){
+	document.getElementById(i).value=c[g[i]];
     document.getElementById(i).innerHTML="<button>"+c[g[i]]+"</button>"
 }
 for(var j=d;j<10;j++){
@@ -33,15 +33,26 @@ for(var j=d;j<10;j++){
 }
 
 }
-var hindi= {
-    "line_12" : ["राम सोया और श्याम भी","श्याम सोया और राम भी","सोया श्याम और राम भम भी"],ा राम और श्या
-    "line_13" : ["मैंने उसे बताया कि राम सो रहा है","मैंने उसे बताया कि सो रहा है राम","उसे सो रहा है","उसे मैंने बताया कि सो रहा है राम","मैंने बताया उसे कि राम सो रहा है","मैंने बताया,"उसे बताया मैंने कि राम सो रहा है","उसे बताया मैंने कि सो रहा है राम","बताया मैंने उसे कि राम उसे कि सो रहा है राम","बताया उसे मैंने कि राम सो रहा है","बताया उसे मैंने कि सो रहा है राम"    "line_14" : ["राम खाकर सोया","खाकर राम सोया","राम सोया खाकर","खाकर सोया राम" खाकर राम"],ाकर","सोया
-    "line_15" : ["बिल्लियों को मारकर कुत्ता सो गया","मारकर बिल्लियों को कुत्ता सो गया"," कुत्ता","मारकर बिल्लियों को सो गया कुत्ता","कुत्ता सो गया बिल्लियों को मारकर","कुत्ता सो गय कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को"],
-    "line_16" : ["एक लाल किताब वहाँ है","एक लाल किताब है वहाँ","वहाँ है एक लाल किताब",किताब"],क लाल
-    "line_17" : ["एक बड़ी सी किताब वहाँ है","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किता एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब"," है वहाँ एक बड़ी  सी एक किताब"]हाँ बड़ी
-};
+else{
+ document.getElementById("dis_sen").innerHTML = "Form a sentence(Declarative or Interrogative or any other type)form the given words";
+ document.getElementById("dis_sub").innerHTML = "(Select the buttons in proper order)";
+
+var hindi= '{"con2":[' +
+         '{"a":"राम और श्याम बाजार गयें", "b":"राम और श्याम गयें बाजार", "c":"बाजार गयें राम और श्याम", "d":"गयें बाजार राम और श्याम "},' +
+      
+        '{"a":"राम सोया और श्याम भी", "b":"श्याम सोया और राम भी", "c":"सोया श्याम और राम भी", "d":"सोया राम और श्याम भी " },' +
+
+          '{"a":"मैंने उसे बताया कि राम सो रहा है", "b":"मैंने उसे बताया कि सो रहा है राम", "c":"उसे मैंने बताया कि राम सो रहा है", "d":"उसे मैंने बताया कि सो रहा है राम", "e":"मैंने बताया उसे कि राम सो रहा है", "f":"मैंने बताया उसे कि सो रहा है राम", "g":"उसे बताया मैंने कि राम सो रहा है", "h":"उसे बताया मैंने कि सो रहा है राम", "i":"बताया मैंने उसे कि राम सो रहा है", "j":"बताया मैंने उसे कि सो रहा है राम<br>", "k":"बताया उसे मैंने कि राम सो रहा है", "l":"बताया उसे मैंने कि सो रहा है राम " },' +
+      
+ '{"a":"राम खाकर सोया", "b":"खाकर राम सोया", "c":"राम सोया खाकर", "d":"खाकर सोया राम", "e":"सोया राम खाकर", "f":"सोया खाकर राम " },' +
+
+    '{"a":"बिल्लियों को मारकर कुत्ता सो गया", "b":"मारकर बिल्लियों को कुत्ता सो गया", "c":"बिल्लियों को मारकर सो गया कुत्ता", "d":"मारकर बिल्लियों को सो गया कुत्ता", "e":"कुत्ता सो गया बिल्लियों को मारकर", "f":"कुत्ता सो गया मारकर बिल्लियों को", "g":"सो गया कुत्ता बिल्लियों को मारकर", "h":"सो गया कुत्ता मारकर बिल्लियों को " },' +
+      
+'{"a":"एक लाल किताब वहाँ है", "b":"एक लाल किताब है वहाँ", "c":"वहाँ है एक लाल किताब", "d":"है वहाँ एक लाल किताब " },' +
+
+            '{"a":"एक बड़ी सी किताब वहाँ है", "b":"एक बड़ी सी किताब है वहाँ", "c":"बड़ी सी एक किताब वहाँ है", "d":" बड़ी सी एक किताब है वहाँ", "e":"वहाँ है एक बड़ी सी किताब", "f":"वहाँ है बड़ी सी एक किताब", "g":" है वहाँ एक बड़ी सी किताब", "h":"है वहाँ बड़ी सी एक किताब " }]}';
 var a=Math.floor(Math.random() * 6);
-obj=JSON.parse(text1);
+obj=JSON.parse(hindi)
 var b=obj.con2[a].a;
 var c=b.split(" ");
 var d=c.length
@@ -56,12 +67,14 @@ m++;
 }
 while (d!=g.length);
 for(var i=0;i<d;i++){
+	document.getElementById(i).value=c[g[i]];
     document.getElementById(i).innerHTML="<button>"+c[g[i]]+"</button>"
 }
 for(var j=d;j<10;j++){
     document.getElementById(j).innerHTML=" "
 }
 
+}
 }
 function select() {
 	let lang = document.getElementById("dropdown").value;
@@ -72,4 +85,21 @@ function select() {
 	} else {
 		alert("Select language");
 	}
-} 
+}
+function display()
+{
+ document.getElementById("p4").innerHTML=" "
+ var y=document.getElementById("lan").value;
+ if(y=="English"){
+ document.getElementById("dis_sen").innerHTML = "Form a sentence(Declarative or Interrogative or any other type)form the given words";
+ document.getElementById("dis_sub").innerHTML = "(Select the buttons in proper order)"
+ }
+}
+function print(i){
+var a=document.getElementById(i).value;
+document.getElementById("p4").innerHTML+=a+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp"+"&nbsp";
+  document.getElementById(i).innerHTML=" ";
+ document.getElementById("dis_sen1").innerHTML= "Formed Sentence";
+document.getElementById("sen2").innerHTML= "(After selecting words)";
+document.getElementById("Reform").innerHTML= "<button>"+"Re-form the sentence"+"</button>";
+}
