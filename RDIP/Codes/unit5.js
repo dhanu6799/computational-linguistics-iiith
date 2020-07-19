f50619aee63bb9dac6f50c0fff6d7e0c41c77a51
dhanu6799 <neelapudhanu@gmail.com>
2020-07-19 20:21:29 +0530

    dropdown

--- RDIP/Codes/unit5.js
+++ RDIP/Codes/unit5.js
@@ -11,3 +11,40 @@ let hindiSentences = ["राम ने सीता के लिए फल त
                       "वाह! वह खूबसूरत है।",
                       "पेड़ से पत्ते गिर गए।"
                      ]
+$("english").hide()
+$("hindi").hide()
+$("sub").hide()
+document.getElementById("msg").innerHTML = ""
+document.getElementById("tbl").innerHTML = " "
+
+let selectLng = document.getElementById("choose-lang")
+function selectLanguage() {
+    if (selectLng.value === "eng") {
+        $("hindi").hide()
+        $('sub").hide()
+        $("english").show()
+        document.getElementById("msg").innerHTML = ""
+        document.getElementById("tbl").innerHTML = " "
+        document.getElementById("english").value = "default"
+        document.getElementById("hindi").value = "default"
+    }
+    else if (selectLng.value === "hnd") {
+        $("english").hide()
+        $("hindi").show()
+        $("sub").hide()
+        document.getElementById("msg").innerHTML = ""
+        document.getElementById("tbl").innerHTML = " "
+        document.getElementById("english").value = "default"
+        document.getElementById("hindi").value = "default"
+    }
+    else {
+        alert("Select Language")
+        $("english").hide()
+        $("hindi").hide()
+        $("sub").hide()
+        document.getElementById("msg").innerHTML = ""
+        document.getElementById("tbl").innerHTML = " "
+        document.getElementById("english").value = "default"
+        document.getElementById("hindi").value = "default"
+    }
+}
